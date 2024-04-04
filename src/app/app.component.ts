81b99be3a4dc024e72352dc905f91fe4ecf51529
abubakar <abubakar.siddik2146@gmail.com>
2024-04-04 17:09:58 +0100

    Add Angular Material dependencies and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list'
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'wishlist';
+  items = [
+    new WishItem('To Learn Angular', 'using angular'),
+    new WishItem('Get Coffee', 'Following Envado Tuts guide', true),
+    new WishItem('Find grass that cuts itself', "I don't know if such thing exist")
+  ];
+  title = 'my wishlist';
 }
---
+++ src/shared/models/wishItem.ts
@@ -0,0 +1,4 @@
+export class WishItem{
+    constructor(public wishText: string, public description: string, public isComplete: boolean = false) {
+}
+}
