65b7d5e545a0f695924e146b07c6efe45d3a754b
abubakar <abubakar.siddik2146@gmail.com>
2024-04-07 19:03:33 +0100

    Update form and input field styles in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 import { CommonModule } from '@angular/common';
-import { MatListModule } from '@angular/material/list';
+import { MatListModule} from '@angular/material/list';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import {MatChipsModule} from '@angular/material/chips';
+
 
 
 
@@ -14,7 +17,7 @@ import {MatInputModule} from '@angular/material/input';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, MatListModule, MatCheckboxModule, MatInputModule, MatButtonModule],
+  imports: [RouterOutlet, CommonModule, MatListModule, MatCheckboxModule, MatInputModule, MatButtonModule, FormsModule,MatChipsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -27,9 +30,39 @@ export class AppComponent {
     new WishItem('Find grass that cuts itself')
   ];
   title = 'my wishlist';
+  listFilter : string = '0';
+  newWishText = '';
+  visibleItems : WishItem[] = this.items;
+
+  addNewWish(){
+    // todo: add wish to item array
+    //clear the textbox
+this.items.push(new WishItem(this.newWishText));
+this.newWishText = '';
+  }
 
   toggleItem(item : WishItem){
     item.isComplete = !item.isComplete;
     console.log(item);
+  }
+
+  filterChanged(value : any){
+    value = Number(value);
+   switch (value){
+    case 0:
+      this.visibleItems = this.items;
+      break;
+      
+      case 1:
+        this.visibleItems = this.items.filter(item => !item.isComplete);
+        break;
+
+        case 2:
+         this.visibleItems = this.items.filter(item => item.isComplete);
+         break;
+
+         default:
+          this.visibleItems = this.items;
+   }
   }
 }
