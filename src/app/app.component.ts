c3bbb166094b5f962cfbd898cde4cf88777a60a7
abubakar <abubakar.siddik2146@gmail.com>
2024-05-10 18:38:24 +0100

    Added Http client to the app
    Added a service called wish.service.ts
    
    added a json file called wishes.json to make a get request and retrieve the data from the json file using OnInit method with where it has getWishes() method defined in the wish.service.ts file.
    
    getWishes() method uses HttpClient to make a get request and retrieve the data from the json file using OnInit method with where it has getWishes() method defined in the wish.service.ts file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component, NgModule } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 import { WishListComponent } from './wish-list/wish-list.component';
 import {EventService} from './../shared/Services/EventService';
 import { WishFilterComponent } from './wish-filter/wish-filter.component';
 import { WishListItemComponent } from './wish-list-item/wish-list-item.component';
-import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
+import {AddWishFormComponent } from './add-wish-form/add-wish-form.component';
+import { WishService } from './wish.service';
 
 
 @Component({
@@ -16,22 +17,24 @@ import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
   styleUrl: './app.component.css'
 })
 
-export class AppComponent {
-  items : WishItem[] = [
-
-    new WishItem('To Learn Angular'),
-    new WishItem('Get Coffee', true),
-    new WishItem('Find grass that cuts itself')
-  ];
+export class AppComponent implements OnInit {
+  items! : WishItem[];
 
   filter: any; 
 
 
-  constructor(events : EventService){
+  constructor(events : EventService, private wishService : WishService ){
     events.listen('removeWish', (wish : any) =>{
       //todo remove wish from items
       let index = this.items.indexOf(wish);
       this.items.splice(index, 1);
     })
   }
+  ngOnInit(): void {
+  this.wishService.getWishes().subscribe((data : any)=> {
+    this.items = data;
+  })
+  }
+
+
 }
