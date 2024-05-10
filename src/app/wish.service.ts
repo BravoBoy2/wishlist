680099d5144819e2c9e31fe3c3f6c6c31ccaed00
abubakar <abubakar.siddik2146@gmail.com>
2024-05-10 19:15:30 +0100

    chore: Add HttpClient and HttpHeaders to wish.service.ts

--- src/app/wish.service.ts
+++ src/app/wish.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { WishItem } from '../shared/models/wishItem';
+import { format } from 'path';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,37 @@ export class WishService {
 
   constructor(private http: HttpClient) {}
 
+
+  private getStandardOptions(): any{
+    return {
+      headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+      })
+  
+    };
+  }
+
      getWishes(){
-      return this.http.get('assets/wishes.json');
+      let options = this.getStandardOptions();
+
+      options.params = new HttpParams({
+        fromObject : {
+          format : 'json'
+        }
+      });
+
+      return this.http.get('assets/wishes.json', options);
     }
+
+    private addWish(wish : WishItem){
+  let options = this.getStandardOptions();
+  options.headers = options.headers.set('Authorization', 'value-need-for-authorization');
+  //post(url, body, options)
+  this.http.post('assets/wishes.json', wish, options);
 }
+
+
+
+}
+
+
