e6ab74b70d53e2c7229c4833796c519feb80d5dc
abubakar <abubakar.siddik2146@gmail.com>
2024-05-10 17:55:22 +0100

    Refactor wish-list.component.html to use conditional rendering for empty wishlist
    Added an event bus system for the remove wishlist
    
    Imported MatIcon class from Material
    
    added some styling for the remove button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,16 @@ import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 import { WishListComponent } from './wish-list/wish-list.component';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatListModule } from '@angular/material/list';
-import { MatInputModule } from '@angular/material/input';
-import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import events from './../shared/Services/EventService';
 import { WishFilterComponent } from './wish-filter/wish-filter.component';
+import { WishListItemComponent } from './wish-list-item/wish-list-item.component';
+import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WishListComponent, MatChipsModule, MatListModule, MatInputModule, AddWishFormComponent, ReactiveFormsModule, WishFilterComponent, FormsModule],
+  imports: [ WishListComponent, RouterOutlet, WishFilterComponent, WishListItemComponent, AddWishFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -26,5 +24,14 @@ export class AppComponent {
     new WishItem('Find grass that cuts itself')
   ];
 
-  filter: any = 0; 
+  filter: any; 
+
+
+  constructor(){
+    events.listen('removeWish', (wish : any) =>{
+      //todo remove wish from items
+      let index = this.items.indexOf(wish);
+      this.items.splice(index, 1);
+    })
+  }
 }
--- src/app/wish-list-item/wish-list-item.component.ts
+++ src/app/wish-list-item/wish-list-item.component.ts
@@ -3,17 +3,19 @@ import { WishItem } from '../../shared/models/wishItem';
 import { MatListModule } from '@angular/material/list';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CommonModule } from '@angular/common';
+import {MatIconModule } from '@angular/material/icon';
+import events from './../../shared/Services/EventService';
 
 @Component({
   selector: 'wish-list-item',
   standalone: true,
-  imports: [MatListModule, MatCheckboxModule, CommonModule],
+  imports: [MatListModule, MatCheckboxModule, CommonModule, MatIconModule],
   templateUrl: './wish-list-item.component.html',
   styleUrl: './wish-list-item.component.css'
 })
 export class WishListItemComponent {
 
-  @Input() wishText! : string;
+  @Input() wish! : WishItem;
   @Input() fullfilled! : boolean;
   @Output() fullfilledChange = new EventEmitter<boolean>();
 
@@ -23,9 +25,12 @@ export class WishListItemComponent {
     return {'strikeout opacity' : this.fullfilled};
 
   }
+
+  removeWish() {
+  events.emit('removeWish', this.wish);
+  }
      toggleFullFilled(){
-    this.fullfilled = !this.fullfilled;
-    this.fullfilledChange.emit(this.fullfilled)
+    this.wish.isComplete = !this.wish.isComplete;
   }
 
 }
