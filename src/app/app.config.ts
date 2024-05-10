5684d1fb7cde354c3c2eed395a458b88a0d11315
abubakar <abubakar.siddik2146@gmail.com>
2024-05-10 18:10:24 +0100

    Refactor app component imports and remove unused dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { EventService } from '../shared/Services/EventService';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(), EventService]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()]
 };
